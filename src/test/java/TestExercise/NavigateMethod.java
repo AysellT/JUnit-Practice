0c1bf0655ce5d52f9b2b91efda4fe3a1b8ba2a6a
AysellT <aysltrg3@gmail.com>
2022-12-28 16:33:10 +0300

    navigate

---
+++ src/test/java/TestExercise/NavigateMethod.java
@@ -0,0 +1,63 @@
+package TestExercise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class NavigateMethod {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1.Yeni bir class olusturalim (Homework)
+        // 2.ChromeDriver kullanarak, facebook sayfasina gidin ve sayfa basliginin (title) “facebook” oldugunu dogrulayin (verify),
+        // degilse dogru basligi yazdirin.
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://facebook.com");
+
+        String expectedKelime = "Facebook";
+        String actualResult = driver.getTitle();
+        if (actualResult.contains(expectedKelime)){
+            System.out.println("Title dogrulandi, title testi PASSED");
+        }else {
+            System.out.println("Title testi FAILED");
+            System.out.println(driver.getTitle());
+        }
+
+        // 3.Sayfa URL’inin “facebook” kelimesi icerdigini dogrulayin, icermiyorsa “actual” URL’i yazdirin.
+        String expectedKelime2 = "facebook";
+        String actualUrl = driver.getCurrentUrl();
+        if (actualUrl.contains(expectedKelime2)){
+            System.out.println("URL testi PASSED");
+        }else{
+            System.out.println("URL testi FAILED");
+            System.out.println(driver.getCurrentUrl());
+        }
+
+        // 4.https://www.walmart.com/ sayfasina gidin.
+        driver.navigate().to("https://www.walmart.com/");
+
+        // 5. Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
+        String expectedKelime3 = "Walmart.com";
+        String actualTitle = driver.getTitle();
+        if (actualTitle.contains(expectedKelime3)){
+            System.out.println("PASSED");
+        }else{
+            System.out.println("FAILED");
+        }
+
+        // 6. Tekrar “facebook” sayfasina donun
+        driver.navigate().back();
+        // 7. Sayfayi yenileyin
+        driver.navigate().refresh();
+        // 8. Sayfayi tam sayfa (maximize) yapin
+        driver.manage().window().maximize();
+
+        // 9.Browser’i kapatin
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
---
+++ src/test/java/TestExercise/NavigateMethodExercise.java
@@ -0,0 +1,76 @@
+package TestExercise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class NavigateMethodExercise {
+
+    public static void main(String[] args) throws InterruptedException {
+        //1. Yeni bir class olusturun (TekrarTesti)
+        //2. Youtube web sayfasına gidin ve sayfa başlığının “youtube” olup olmadığını
+        //doğrulayın (verify), eğer değilse doğru başlığı(Actual Title) konsolda yazdirin.
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.youtube.com");
+
+        String expectedResult = "youtube";
+        String actualResult = driver.getTitle();
+
+        if (actualResult.equalsIgnoreCase(expectedResult)) {
+            System.out.println(" youtube Title testi PASSED");
+        } else {
+            System.out.println("youtube Title testi FAILED");
+            System.out.println(actualResult);
+        }
+
+        //3. Sayfa URL'sinin “youtube” içerip içermediğini (contains) doğrulayın, içermiyorsa
+        //doğru URL'yi yazdırın.
+        String expectedKelime2 = "youtube";
+        String actualURL = driver.getCurrentUrl();
+        if (actualURL.contains(expectedKelime2)) {
+            System.out.println("Youtube URL testi PASSED");
+        } else {
+            System.out.println("Youtube URL testi FAILED");
+            System.out.println(actualURL);
+        }
+
+        //4. Daha sonra Amazon sayfasina gidin https://www.amazon.com/
+        driver.navigate().to("https://www.amazon.com");
+        //5. Youtube sayfasina geri donun
+        driver.navigate().back();
+        // 6. Sayfayi yenileyin
+        driver.navigate().refresh();
+        //7. Amazon sayfasina donun
+        driver.navigate().forward();
+        //8. Sayfayi tamsayfa yapin
+        driver.manage().window().maximize();
+        //9. Ardından sayfa başlığının "Amazon" içerip içermediğini (contains) doğrulayın, Yoksa doğru başlığı(Actual Title) yazdırın.
+        String expectedTitle = "Amazon";
+        String actualTitle = driver.getTitle();
+        if (actualTitle.contains(expectedTitle)) {
+            System.out.println("Amazon title testi PASSED");
+        } else {
+            System.out.println("Amazon title testi FAILED");
+            System.out.println(actualTitle);
+        }
+
+        //10.Sayfa URL'sinin https://www.amazon.com/ olup olmadığını doğrulayın, degilse doğru URL'yi yazdırın
+        String expectedAmazonUrl = "https://www.amazon.com/";
+        String actualAmazonUrl = driver.getCurrentUrl();
+        if (actualAmazonUrl.equals(expectedAmazonUrl)) {
+            System.out.println("Amazon url Testi PASSED");
+        } else {
+            System.out.println("Amazon url testi FAILED");
+            System.out.println(actualAmazonUrl);
+        }
+
+        //11.Sayfayi kapatin
+        Thread.sleep(3000);
+        driver.close();
+
+
+    }
+}
