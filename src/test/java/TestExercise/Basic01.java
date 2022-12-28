582df887e33cadabd327c01c4ca219c86c58db36
AysellT <aysltrg3@gmail.com>
2022-12-28 11:15:08 +0300

    ilk versiyon

---
+++ src/test/java/TestExercise/Basic01.java
@@ -0,0 +1,33 @@
+package TestExercise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Basic01 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com.tr");
+        System.out.println(driver.getTitle()); //Amazon.com. Spend less. Smile more.
+
+        String expectedKelime = "alisveris";
+        String pageSource = driver.getPageSource();
+
+        if (pageSource.contains(expectedKelime)){
+            System.out.println("test passed");
+        }else{
+            System.out.println("test failed");
+        }
+
+        Thread.sleep(3000);
+        driver.close();
+
+    }
+}
---
+++ src/test/java/TestExercise/Basic03.java
@@ -0,0 +1,32 @@
+package TestExercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Basic03 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("nutella");
+
+        WebElement araButon = driver.findElement(By.id("nav-search-submit-button"));
+        araButon.click();
+
+        Thread.sleep(3000);
+        driver.close();
+
+    }
+
+}
