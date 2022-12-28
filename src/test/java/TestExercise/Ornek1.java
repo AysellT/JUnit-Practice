a7d4b0041d6c6221c30807d58067436cb6629c12
AysellT <aysltrg3@gmail.com>
2022-12-28 16:36:34 +0300

    Locators

---
+++ src/test/java/TestExercise/Ornek1.java
@@ -0,0 +1,41 @@
+package TestExercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Ornek1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        //1 - Bir test class'i olusturun ilgili ayarlari yapin
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //2- https://www.automationexercise.com/adresine gidin
+        driver.get("https://www.automationexercise.com");
+
+        //3- Category bolumundeki elementleri locate edin
+        List<WebElement> kategoriListesi = driver.findElements(By.className("panel-heading"));
+
+        //4- Category bolumunde 3 element oldugunu test edin
+        if (kategoriListesi.size()==3){
+            System.out.println("test PASSED");
+        }else{
+            System.out.println("test FAILED");
+        }
+
+        //5- Category isimlerini yazdirin
+        for (WebElement each :kategoriListesi){
+            System.out.print(each.getText()+" ");
+        }
+        //6- Sayfayi kapatin
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
---
+++ src/test/java/TestExercise/Ornek2.java
@@ -0,0 +1,47 @@
+package TestExercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Ornek2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //1- https://www.amazon.com/ sayfasına gidin.
+        driver.get("https://www.amazon.com");
+
+        // 2- Arama kutusuna “city bike” yazip aratin
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("city bike"+ Keys.ENTER);
+
+        // 3- Görüntülenen sonuçların sayısını yazdırın
+        WebElement bikeSayisi = driver.findElement(By.xpath("(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+
+        String sonucText = bikeSayisi.getText();
+
+        String [] sonucTextArr = sonucText.split(" ");
+
+        String sonucSayisi = sonucTextArr[2]; //"121"
+
+        int sonucSayisiInt = Integer.parseInt(sonucSayisi);//121
+
+        System.out.println(sonucSayisiInt+" adet sonuc bulundu");
+
+
+        // 4- Listeden ilk urunun resmine tıklayın.
+        driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-fixed-height'])[1]")).click();
+
+
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
