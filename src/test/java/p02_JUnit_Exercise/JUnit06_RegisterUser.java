ba834358243da2afcddbb9f0bd9da2b837123550
AysellT <aysltrg3@gmail.com>
2023-01-02 17:19:14 +0300

    Exercise

--- src/test/java/p02_JUnit_Exercise/JUnit06_RegisterUser.java
+++ src/test/java/p02_JUnit_Exercise/JUnit06_RegisterUser.java
@@ -59,15 +59,19 @@ public class JUnit06_RegisterUser {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
+
         //3. Verify that home page is visible successfully
         String expectedTitle = "Automation Exercise";
         String actualTitle = driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedTitle));
+
         //4. Click on 'Signup / Login' button
         driver.findElement(By.xpath("//a[text()=' Signup / Login']")).click();
+
         //5. Verify 'New User Signup!' is visible
         WebElement newUserSignupElementi = driver.findElement(By.xpath("//h2[text()='New User Signup!']"));
         Assert.assertTrue(newUserSignupElementi.isDisplayed());
+
         //6. Enter name and email address
         WebElement name = driver.findElement(By.xpath("//input[@name='name']"));
         Actions actions = new Actions(driver);
@@ -76,11 +80,14 @@ public class JUnit06_RegisterUser {
                 .sendKeys(faker.name().firstName())
                 .sendKeys(Keys.TAB)
                 .sendKeys(faker.internet().emailAddress()).perform();
+
         //7. Click 'Signup' button
         driver.findElement(By.xpath("//button[text()='Signup']")).click();
+
         //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
         WebElement enterAccountElementi = driver.findElement(By.xpath("//b[text()='Enter Account Information']"));
         Assert.assertTrue(enterAccountElementi.isDisplayed());
+
         //9. Fill details: Title, Name, Email, Password, Date of birth
         WebElement genderTitle = driver.findElement(By.id("uniform-id_gender2"));
         actions.click(genderTitle)
@@ -98,10 +105,13 @@ public class JUnit06_RegisterUser {
                 .sendKeys("2000")
                 .sendKeys(Keys.TAB)
                 .sendKeys(Keys.PAGE_DOWN).perform();
+
         //10. Select checkbox 'Sign up for our newsletter!'
         driver.findElement(By.id("newsletter")).click();
+
         //11. Select checkbox 'Receive special offers from our partners!'
         driver.findElement(By.id("optin")).click();
+
         //12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
         WebElement firstNameKutusu = driver.findElement(By.id("first_name"));
         actions.click(firstNameKutusu)
@@ -124,18 +134,24 @@ public class JUnit06_RegisterUser {
                 .sendKeys(faker.address().zipCode())
                 .sendKeys(Keys.TAB)
                 .sendKeys(faker.phoneNumber().cellPhone()).perform();
+
         //13. Click 'Create Account button'
         driver.findElement(By.xpath("//button[text()='Create Account']")).click();
+
         //14. Verify that 'ACCOUNT CREATED!' is visible
         WebElement accountCreated =driver.findElement(By.xpath("//*[text()='Account Created!']"));
         Assert.assertTrue(accountCreated.isDisplayed());
+
         //15. Click 'Continue' button
         driver.findElement(By.xpath("//a[text()='Continue']")).click();
+
         //16. Verify that 'Logged in as username' is visible
         WebElement loggedInElementi = driver.findElement(By.xpath("//*[text()=' Logged in as ']"));
         Assert.assertTrue(loggedInElementi.isDisplayed());
+
         //17. Click 'Delete Account' button
         driver.findElement(By.xpath("//a[text()=' Delete Account']")).click();
+
         //18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
         WebElement accountDeleted = driver.findElement(By.xpath("//*[text()='Account Deleted!']"));
         Assert.assertTrue(accountDeleted.isDisplayed());
